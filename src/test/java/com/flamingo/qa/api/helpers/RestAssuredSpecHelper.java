97f96de31cd9ff3f1b0e3470e6003b08fa16512b
giokur <george.kurtanidze+1@gmail.com>
2025-12-10 19:41:49 +0100

    add logging

--- src/test/java/com/flamingo/qa/api/helpers/RestAssuredSpecHelper.java
+++ src/test/java/com/flamingo/qa/api/helpers/RestAssuredSpecHelper.java
@@ -2,8 +2,9 @@ package com.flamingo.qa.api.helpers;
 
 import com.flamingo.qa.api.config.DefaultSettings;
 import com.flamingo.qa.api.models.AuthToken;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import io.restassured.specification.RequestSpecification;
@@ -11,21 +12,23 @@ import io.restassured.specification.RequestSpecification;
 public class RestAssuredSpecHelper {
 
     public static RequestSpecification unauthorizedSpec() {
-        return new RequestSpecBuilder()
-                .setBaseUri(getBaseUri())
-                .setContentType(ContentType.JSON)
-                .build();
+        return specBuilder().build();
     }
 
     public static RequestSpecification authorizedSpec(AuthToken authToken) {
         Cookie cookie = new Cookie.Builder("token", authToken.getToken()).build();
+        return specBuilder()
+                .setAccept("application/json")
+                .addCookie(cookie)
+                .build();
+    }
+
+    private static RequestSpecBuilder specBuilder() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         return new RequestSpecBuilder()
                 .setBaseUri(getBaseUri())
                 .setContentType(ContentType.JSON)
-                .setAccept("application/json")
-                .addCookie(cookie)
-                .log(LogDetail.ALL)
-                .build();
+                .addFilter(new AllureRestAssured());
     }
 
     private static String getBaseUri() {
